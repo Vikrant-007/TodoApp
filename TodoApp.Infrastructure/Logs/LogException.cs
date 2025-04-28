b9d63a05b9bf47976c3c526b17fa6ff504aeac99
Vikrant-007 <vikrantsaini2106@gmail.com>
2025-04-28 15:57:37 +0530

    validation and genric response setup

---
+++ TodoApp.Infrastructure/Logs/LogException.cs
@@ -0,0 +1,20 @@
+﻿using Serilog;
+
+namespace TodoApp.Infrastructure.Logs
+{
+	public static class LogException 
+	{
+		public static void LogExceptions(Exception ex)
+		{
+			LogToFile(ex.Message);
+			LogToConsole(ex.Message);
+			LogToDebug(ex.Message);
+		}
+
+		private static void LogToDebug(string message) => Log.Debug(message);
+
+		private static void LogToConsole(string message) => Log.Warning(message);
+
+		private static void LogToFile(string message) => Log.Error(message);
+	}
+}
